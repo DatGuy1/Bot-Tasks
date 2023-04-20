311e366d400af1803536d4b49d7d045c606c6b3d
DatGuy <datguysteam@gmail.com>
2023-04-20 17:32:46 +0000

    Simplify starting rust task

--- start_rust_task.sh
+++ start_rust_task.sh
@@ -2,5 +2,6 @@
 set -eu
 
 cd "$1";
-readarray -d '/' -t split_path <<<"$1/"; unset 'split_path[-1]'; declare -p split_path;
-"$1/target/release/${split_path[-1]}";
+# readarray -d '/' -t split_path <<<"$1/"; unset 'split_path[-1]'; declare -p split_path;
+# "$1/target/release/${split_path[-1]}";
+"$1/target/release/$(basename $1)"

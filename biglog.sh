e8dd386d1fbb9c6be070d23c345c59024d5d2990
DatGuy <datguysteam@gmail.com>
2024-01-03 14:56:12 +0000

    chore: don't remove log files
    Is now handled by toolforge-jobs

--- biglog.sh
+++ biglog.sh
@@ -1,10 +1,3 @@
-rm wikiwork.out
-rm wikiwork.log
-rm imageresizer.out
-rm afreporter.err
-rm footy.err
-rm playerstats.out
-rm playerstats.err
 find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.png" -type f -delete
 find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.jpg" -type f -delete
 find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.jpeg" -type f -delete
@@ -12,11 +5,3 @@ find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.webp" -typ
 find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.tif" -type f -delete
 find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.svg" -type f -delete
 find "/data/project/datbot/Tasks/NonFreeImageResizer/files" -iname "*.gif" -type f -delete
-sleep 3
-touch afreporter.err
-touch imageresizer.out
-touch wikiwork.out
-touch wikiwork.log
-touch footy.out
-touch playerstats.out
-touch playerstats.err
